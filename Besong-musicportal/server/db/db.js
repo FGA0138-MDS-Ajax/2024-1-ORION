9f955b77a305952c12c4904a1f1fafbc149710ce
Brenno-Silva01 <brenno877@gmail.com>
2024-06-14 15:21:56 -0300

    atualizando db

--- Besong-musicportal/server/db/db.js
+++ Besong-musicportal/server/db/db.js
@@ -1,15 +1,23 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Fernoronh@25',
-    database: 'music_portal'
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'sua-senha',
+  database: 'besong',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL Database.');
-});
+// Verifica se a conexão foi estabelecida com sucesso
+pool.getConnection()
+  .then(connection => {
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+    connection.release(); // libera a conexão de teste
+  })
+  .catch(err => {
+    console.error('Erro ao conectar com o banco de dados:', err.message);
+  });
 
-module.exports = connection;
+module.exports = pool;
