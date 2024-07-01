c89d9d97c8434dc5d34ec389d331b132beed279c
Victor Andreozzi <victor.andreozzi.dev@outlook.com>
2024-07-01 04:36:20 -0300

    integração do cadastro do contratante

---
+++ Besong-musicportal/public/scripts/scriptContratante.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('musicoForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const nome = document.getElementById('contratanteNome').value;
+        const usuario = document.getElementById('usuario').value;
+        const telefone = document.getElementById('contratanteTelefone').value;
+        const email = document.getElementById('contratanteEmail').value;
+        const regiao = document.getElementById('contratanteRegiao').value;
+        const senha = document.getElementById('senha').value;
+        
+     
+
+        fetch('http://54.242.81.142:89/api/contratantes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 
+                nome, 
+                usuario, 
+                telefone, 
+                email,
+                regiao, 
+                senha
+                
+                
+            }),
+        })
+        .then(response => response.text())
+        .then(data => {
+            alert(data); // Display response message or handle success
+            document.getElementById('musicoForm').reset(); // Reset the form fields
+            window.location.href = './login.html';
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+        
+    });
+});
---
+++ Besong-musicportal/server/api/cadastroContratante/contratanteApi.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const app = express();
+
+// Middleware para permitir CORS
+app.use(cors());
+
+// Configuração do banco de dados usando createPool do mysql2
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "54.242.81.142",    // Host do seu banco de dados MySQL
+  port: 3306,                // Porta do seu banco de dados MySQL
+  user: "root",              // Usuário do seu banco de dados MySQL
+  password: "1234",          // Senha do seu banco de dados MySQL
+  database: "beSongDB",      // Nome do banco de dados
+  multipleStatements: true
+});
+
+// Middleware para parse de JSON
+app.use(express.json());
+
+// Rota para receber dados do formulário e inserir no banco de dados
+app.post('/api/contratantes', async (req, res) => {
+  const userData = req.body;
+
+  // Log dos valores recebidos
+  console.log('Dados recebidos do formulário:');
+  console.log(userData);
+
+
+  try {
+    // Hashear a senha do usuário
+    const hashedPassword = await bcrypt.hash(userData.senha, 10);
+
+    // Montar query SQL para inserção
+    const sql = `
+      INSERT INTO CONTRATANTE
+      (nome, email, usuario, senha, regiao,telefone)
+      VALUES (?, ?, ?, ?, ?, ?)
+    `;
+    const values = [
+      userData.nome,
+      userData.email,
+      userData.usuario,
+      hashedPassword, // Usar a senha hasheada
+      userData.regiao,
+      userData.telefone
+     
+    ];
+
+    // Executar a query usando pool.query do mysql2
+    pool.query(sql, values, (err, result) => {
+      if (err) {
+        console.error('Erro ao executar a query: ' + err.stack);
+        res.status(500).json({ error: 'Erro interno ao salvar no banco de dados' });
+        return;
+      }
+      console.log('Registro inserido com sucesso');
+      res.json({ message: `Parabéns, ${userData.nome}! Cadastro efetuado com sucesso.` });
+    });
+  } catch (err) {
+    console.error('Erro ao hashear a senha: ' + err.stack);
+    res.status(500).json({ error: 'Erro interno ao processar a senha' });
+  }
+});
+
+// Iniciar o servidor na porta 80
+const PORT = process.env.PORT || 89;
+app.listen(PORT, () => {
+  console.log(`Servidor está rodando na porta ${PORT}`);
+});
